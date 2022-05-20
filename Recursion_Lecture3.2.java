9ca60b30ecb42847109a7d6f732263d30fdc3e59
Ujjwal Shukla <73957886+Ujjwal7017@users.noreply.github.com>
2022-05-20 23:36:55 +0530

    Create Recursion_Lecture3.2.java

---
+++ Recursion_Lecture3.2.java
@@ -0,0 +1,16 @@
+// Ques : Write a Program to print the fibonacci series .
+
+import java.util.*;
+class Test2{
+    public static int fibo(int n){
+        if(n == 1 || n==2){
+            return 1;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(fibo(n));
+	}
+}
