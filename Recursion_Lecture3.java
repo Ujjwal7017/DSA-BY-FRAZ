b16d2a64580fb1ce0077371ea915f52883c6f974
Ujjwal Shukla <73957886+Ujjwal7017@users.noreply.github.com>
2022-05-20 23:35:28 +0530

    Create Recursion_Lecture3.java

---
+++ Recursion_Lecture3.java
@@ -0,0 +1,17 @@
+//Ques : Write a Program to Print the Sum of n natural Numbers .
+  
+  import java.util.*;
+
+class Test2 {
+    public static int sum(int n){
+        if(n == 1){
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(sum(n));
+	}
+}
